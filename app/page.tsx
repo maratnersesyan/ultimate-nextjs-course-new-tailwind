0fddd84480b60b99a85c8c0b9ff8c4a70ae43061
Adrian <contact@jsmastery.pro>
2025-05-01 12:15:59 +0200

    Implemnet ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js 👋</h1>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next.js 👋
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
